93bcb2b83304908a78b1a668e4ac1d16ef9afe07
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-11 22:06:59 +0100

    First commit

---
+++ Figuras/Figuras.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figuras
+{
+    class Figuras
+    {
+        public decimal area {get; set;} // get y set son de tipo lectura y escritura, sintaxis para declarar un atributo
+        decimal lado;
+        decimal ancho;
+        decimal radio;
+
+        public void areaCuadrado(lado, area)
+        {
+            Console.WriteLine("Escribe el lado: ");
+            lado = Convert.ToDecimal (Console.ReadLine());
+            area = lado * lado;
+            Console.WriteLine("El resultado del área es: " + area);
+        }
+    
+    }
+}
---
+++ Figuras/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Figuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figuras figuras = new Figuras(); // instanciando a la clase Figuras
+            figuras.areaCuadrado(); // accedemos al método creado
+            Console.ReadKey();
+        }
+    }
+}
