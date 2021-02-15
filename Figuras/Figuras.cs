d31cc43163941e17c550b5b0fec4f0787ad15650
Claudia Torres Hernández <claudiatorreshernandez88@gmail.com>
2021-02-15 20:01:18 +0100

    Añadir funcionalidades a la clases Figuras y Program.

--- Figuras/Figuras.cs
+++ Figuras/Figuras.cs
@@ -6,18 +6,35 @@ namespace Figuras
 {
     class Figuras
     {
-        public decimal area {get; set;} // get y set son de tipo lectura y escritura, sintaxis para declarar un atributo
-        decimal lado;
-        decimal ancho;
-        decimal radio;
+        // public decimal area {get; set;} // get y set son de tipo lectura y escritura, sintaxis para declarar un atributo
+        double area = 0;
+        double lado = 0;
+        double ancho = 0;
+        double radio = 0;
 
-        public void areaCuadrado(lado, area)
+        public void areaCuadrado() // sintaxis: visibilidad + tipo de valor a retornar + nombre de la función + argumento (opcional)
         {
             Console.WriteLine("Escribe el lado: ");
-            lado = Convert.ToDecimal (Console.ReadLine());
+            lado = Convert.ToDouble (Console.ReadLine());
             area = lado * lado;
             Console.WriteLine("El resultado del área es: " + area);
         }
-    
+        public void areaRectangulo()
+        {
+            Console.WriteLine("Escribe el largo: ");
+            lado = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Escribe el ancho: ");
+            ancho = Convert.ToDouble(Console.ReadLine());
+            area = lado * ancho;
+            Console.WriteLine("El resultado del área es: " + area);
+        }
+        public void areaCirculo()
+        {
+            Console.WriteLine("Escribe el radio: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI*Math.Pow(radio, 2);
+            Console.WriteLine("El resultado del área es: " + area);
+        }
+
     }
 }
--- Figuras/Program.cs
+++ Figuras/Program.cs
@@ -6,9 +6,51 @@ namespace Figuras
     {
         static void Main(string[] args)
         {
-            Figuras figuras = new Figuras(); // instanciando a la clase Figuras
-            figuras.areaCuadrado(); // accedemos al método creado
-            Console.ReadKey();
+            try
+            {
+                int n = 1;
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("¿Desea utilizar la aplicación para el cálculo del área de alguna figura? <S/N>");
+                    string valor = Console.ReadLine();
+
+                    //if (valor == "S" || valor == "s") // || es igual a "or"
+                    if (valor.ToUpper()=="S")
+                    {
+                        Console.Write("Elija la figura a la que le desea calcular el área: \n" +
+                         "1. Área de un cuadrado \n" + "2. Área de un rectangulo \n" + "3. Área de un circulo \n");
+                        string opcion = Console.ReadLine();
+
+                        Figuras figuras = new Figuras(); // instanciando a la clase Figuras
+                        switch (opcion)
+                        {
+                            case "1": figuras.areaCuadrado(); break;
+                            case "2": figuras.areaRectangulo(); break;
+                            case "3": figuras.areaCirculo(); break;
+                            default:
+                                Console.Write("La opción elegida no es válida");
+                                break;
+                        }
+                        n++;
+                    }
+                    else if (valor == "N" || valor == "n")
+                    {
+                        return; // retorna a la función principal y se termina la ejecución en curso (se cierra la aplicación)
+                    }
+ 
+                }
+ 
+                Console.ReadKey();
+
+                //figuras.areaCuadrado(); // accedemos al método creado
+                //figuras.areaRectangulo();
+                //figuras.areaCirculo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("La aplicación ha fallado. Contacte con su técnico" + ex.Message);
+                // throw;
+            }
         }
     }
 }
